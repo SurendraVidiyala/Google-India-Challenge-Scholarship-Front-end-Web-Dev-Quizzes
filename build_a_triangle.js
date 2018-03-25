b7ec5003780aa13efe92db255268f639d09427b7
Rajat Gupta <imrajatgupta@gmail.com>
2018-03-25 18:14:30 +0530

    create build_a_triangle.js using for loop

---
+++ build_a_triangle.js
@@ -0,0 +1,36 @@
+/*
+ * Programming Quiz: Build A Triangle (5-3)
+ 
+OUTPUT:
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * * 
+* * * * * * * 
+* * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * * *
+ */
+
+// creates a line of * for a given length
+function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
+}
+
+// your code goes here.  Make sure you call makeLine() in your own code.
+function buildTriangle(length) {
+    var line = "";
+    for(var i = 1; i<= length; i++) {
+        line = line + makeLine(i);
+    }
+    return line;
+}
+
+console.log(buildTriangle(10));
