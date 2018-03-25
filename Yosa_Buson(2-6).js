1a6492b2dd63adc10f382b9d5dfc8937ccbb6bad
Rajat Gupta <imrajatgupta@gmail.com>
2018-03-25 16:30:59 +0530

    create Yosa_Buson_(2-6).js

---
+++ Yosa_Buson(2-6).js
@@ -0,0 +1,14 @@
+
+
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ 
+ * Build a string using concatenation by combining the lines from this famous haiku poem by Yosa Buson.
+OUTPUT:
+  Blowing from the west
+  Fallen leaves gather
+  In the east.
+ */
+
+var haiku = "Blowing from the west\n" + "Fallen leaves gather\n" + "In the east.";
+console.log(haiku);
