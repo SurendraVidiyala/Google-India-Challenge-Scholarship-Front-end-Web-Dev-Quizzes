cd6f8211adb001cab461bb7ef14bc5a53720a47d
Rajat Gupta <imrajatgupta@gmail.com>
2018-03-25 18:11:36 +0530

    create simple_factorials.js

---
+++ simple_factorials.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+var num = 12;
+var solution = 1;
+for(var i=1; i <= num; i++) {
+    solution = solution * i;
+}
+console.log(solution);
