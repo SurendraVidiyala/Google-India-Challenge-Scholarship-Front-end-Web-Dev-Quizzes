62cd36fa4739c0a661cf5050c0f892297947d177
Rajat Gupta <imrajatgupta@gmail.com>
2018-03-25 16:29:34 +0530

    fix: output format added in comment

--- all_tied_up.js
+++ all_tied_up.js
@@ -1,5 +1,9 @@
 /*
  * Programming Quiz: All Tied Up (2-5)
+ * Build a single string that resembles the following joke.
+OUTPUT:
+Why couldn't the shoes go out and play?
+They were all "tied" up!
  */
 
 var joke = "Why couldn't the shoes go out and play?\nThey were all \"tied\" up!";
